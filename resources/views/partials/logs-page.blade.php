f682ab350d66cce6b38074b61f59d279a440918e
Orlando Villanueva <orlandovillanueva11@gmail.com>
2025-10-20 18:35:47 -0400

    Align history navigation labels with page title

--- resources/views/partials/logs-page.blade.php
+++ resources/views/partials/logs-page.blade.php
@@ -1,7 +1,7 @@
 {{-- Logs Page Container Partial (No Sidebar) --}}
 {{-- This partial is loaded via HTMX navigation and includes the page-container structure --}}
 
-<title>History - {{ config('app.name') }}</title>
+<title>Reading History - {{ config('app.name') }}</title>
 
 <!-- Full-width Content when no sidebar is defined -->
 <div class="flex-1 p-4 lg:p-6 lg:pb-6 container">
