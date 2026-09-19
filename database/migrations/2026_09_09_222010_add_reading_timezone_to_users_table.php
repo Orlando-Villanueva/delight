53e705c6ed1567da6aca745a983a343cc76dc521
Orlando Villanueva <orlandovillanueva11@gmail.com>
2026-09-19 18:19:49 -0400

    [DEL-318] Type timezone migration callbacks

--- database/migrations/2026_09_09_222010_add_reading_timezone_to_users_table.php
+++ database/migrations/2026_09_09_222010_add_reading_timezone_to_users_table.php
@@ -8,14 +8,14 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table): void {
             $table->string('reading_timezone')->nullable();
         });
     }
 
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table): void {
             $table->dropColumn('reading_timezone');
         });
     }
